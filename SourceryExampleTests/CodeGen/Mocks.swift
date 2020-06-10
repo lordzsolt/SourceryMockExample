e4376a14379b61dcb394e3da0ceb2df05ee7f3a2
Zsolt Kovacs <lordzsoltt@gmail.com>
2020-06-11 01:32:11 +0200

    Add Mock template to the project

---
+++ SourceryExampleTests/CodeGen/Mocks.swift
@@ -0,0 +1,10 @@
+//
+//  AutoMockable.swift
+//  SourceryExampleTests
+//
+//  Created by Zsolt Kovacs on 08.11.20.
+//  Copyright © 2020 iosmith. All rights reserved.
+//
+
+import Foundation
+@testable import SourceryExample
